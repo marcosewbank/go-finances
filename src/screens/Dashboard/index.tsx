4e83c5e28cdc5a106fb9c0cd94cd1bda5a58b4ac
[marcos ewbank] <marcos.suarez@pivotree.com>
2022-07-11 17:35:20 -0300

    finish flat list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+
 import { HighlightCard } from "../../components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/TransactionCard";
 
 import {
   Container,
@@ -12,9 +17,43 @@ import {
   UserWrapper,
   Icon,
   HighlightCards,
+  TransactionList,
+  Transactions,
+  Title,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export const Dashboard = () => {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de Site",
+      amount: "R$ 12.000,00",
+      date: "13/04/2020",
+      category: { name: "vendas", icon: "dollar-sign" },
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Aluguel de teste",
+      amount: "R$ 1.000,00",
+      date: "13/04/2020",
+      category: { name: "Alimentação", icon: "coffee" },
+    },
+    {
+      id: "3",
+      type: "positive",
+      title: "Desenvolvimento de jogos",
+      amount: "R$ 199.000,00",
+      date: "13/04/2022",
+      category: { name: "vendas", icon: "dollar-sign" },
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -56,6 +95,15 @@ export const Dashboard = () => {
           lastTransaction="01 à 16 de abril"
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 };
